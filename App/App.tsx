1c4cd479d8e5e2b577ea8cd25eb0d41a14ce1dfb
KJY2523 <51241792+KJY2523@users.noreply.github.com>
2025-07-15 19:26:33 +0900

    Update App.tsx

--- App/App.tsx
+++ App/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
-import { StatusBar } from "react-native";
+import { StatusBar, SafeAreaView, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <>
-      <StatusBar hidden={false} />
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#fff" barStyle="dark-content" />
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
-    </>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff", // 앱 배경색에 맞게 조정
+  },
+});
