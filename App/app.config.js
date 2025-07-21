3ac44250a46e5e73063bc05d40ae2806af9048c0
Kim Juha <westa0514@naver.com>
2025-07-22 00:21:09 +0900

    feat: 환경변수 설정

---
+++ App/app.config.js
@@ -0,0 +1,43 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "App",
+    slug: "App",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSCameraUsageDescription: "얼굴 인증을 위해 카메라 접근이 필요합니다.",
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    fonts: [
+      {
+        asset: "./assets/fonts/Roboto-VariableFont_wdth,wght.ttf",
+        family: "Roboto",
+      },
+    ],
+    extra: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
+  },
+};
